948d9096721ecfbc3b169e4ab2f88842a61eda95
RenanFelixDeSouza <renan050900@gmail.com>
2023-08-16 23:06:31 -0400

    pagina home

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,6 @@ import styles from './Footer.module.css';
 const Footer = () => {
     return (<footer className={styles.footer}>
         <h3>Aqui é o fim da página.</h3>
-        <p>Criador: Renan Felix de Souza   <break/>
-            <a href='https://www.linkedin.com/in/renan-felix-de-souza/' target="_blank" rel="noopener noreferrer">
-              Linkedin
-            </a> |   <break/>
-            <a href='https://github.com/RenanFelixDeSouza/' target="_blank" rel="noopener noreferrer">
-                  <break/> GitHub
-            </a>
-        </p>
-        <p>DevKids &copy; 2022</p>
     </footer>
     );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,18 @@
-//css
+import fazendeiro from '../../img/fazendeiro.png'
+import React from 'react';
+import styles from './Home.module.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={styles.home}>
+            <div className={styles.box}>
+                <div className={styles.boxText}>
+                    <p>NF-e, CT-e, MDF-e.</p>
+                    <h1>Produtor Rural, emita nota fiscal eletrônica com o sistema da Geatech</h1>
+                    <button className={styles.active}>FALE CONOSCO</button>
+                </div>
+                <img src={fazendeiro} alt="Imagem lateral" className={styles.fazendeiro} />
+            </div>
         </div>
     );
 };
